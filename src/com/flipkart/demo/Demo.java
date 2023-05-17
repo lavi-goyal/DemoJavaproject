a465ad1f4ac11d949a3ed93a955f5ca31a05e2f1
Lavi Goyal <lavi.goyal@BLRETV-GL888MD6M.local>
2023-05-17 17:28:39 +0530

    first commit

---
+++ src/com/flipkart/demo/Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author lavi.goyal
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first programme");
+	}
+
+}
